4624567a34f69287d4f6791adae23ac7f34d3f13
Ziyike1 <tul51428@temple.edu>
2024-09-09 14:48:09 +0800

    edit alien_invasion.py to import settings and ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,10 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
+
 class AlienInvasion:
     """管理游戏资源和行为的类"""
 
@@ -8,8 +12,15 @@ class AlienInvasion:
         """初始化游戏并创建游戏资源"""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((800, 600))
+        self.clock = pygame.time.Clock()
+        # self.screen = pygame.display.set_mode((800, 600))
+        # self.bg_color = (200, 200, 200)
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption('Alien Invasion')
+        self.ship = Ship(self)
 
 
     def run_game(self):
@@ -20,8 +31,14 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
+            """每次循环时都会重绘屏幕"""
+            # self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
+
             """让最近绘制的屏幕可见"""
             pygame.display.flip()
+            self.clock.tick(60)
 
 
 if __name__ == '__main__':
